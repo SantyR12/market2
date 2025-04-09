92cc0f62e3344866303164a33b9a713970da3750
Santiago <santiago101ramoss@gmail.com>
2025-04-09 11:55:59 -0500

    obtener por id

--- src/main/java/com/example/market/domain/dto/OrderItemDTO.java
+++ src/main/java/com/example/market/domain/dto/OrderItemDTO.java
@@ -1,10 +1,13 @@
 package com.example.market.domain.dto;
 
+import com.example.market.infraestructura.entity.Orden;
+
 public class OrderItemDTO {
 
     private long id;
     private Integer quantity;
     private Double unitPrice;
+    private Orden orden;
 
     public Integer getQuantity() {
         return quantity;
--- src/main/java/com/example/market/domain/repository/IClient.java
+++ src/main/java/com/example/market/domain/repository/IClient.java
@@ -1,5 +1,6 @@
 package com.example.market.domain.repository;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.market.domain.dto.ClientDTO;
 
@@ -7,7 +8,7 @@ import com.example.market.domain.dto.ClientDTO;
 public interface IClient {
 
     List<ClientDTO> getAll();
-
+    Optional<ClientDTO> getById(Long id);
     void create(ClientDTO clientDTO);
     void update(ClientDTO clientDTO);
     void delete(Long id);
